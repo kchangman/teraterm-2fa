197a7fa7ffad065f249b02923790ec705b5eb1ad
unknown <k_nagai@km.in.nttpc.co.jp>
2025-03-18 11:00:51 +0900

    combine config file

--- generate_otp.py
+++ generate_otp.py
@@ -1,17 +1,7 @@
 import sys
 import pyotp
-import configparser
-import os
 
-def read_config():
-    config = configparser.ConfigParser()
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    config_path = os.path.join(script_dir, 'config.ini')
-    config.read(config_path)
-    return config
-
-config = read_config()
-secret = config['auth']['secret']
+secret = sys.argv[2]
 
 totp = pyotp.TOTP(secret)
 otp = totp.now()
